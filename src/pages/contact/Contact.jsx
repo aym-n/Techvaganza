8af36d4ae9f087fb4f10f9a2b6272557a16908b5
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-09-07 06:40:00 +0530

    added contact card

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,45 @@
 import PageLayout from "../../Components/PageLayout";
+import ContactCard from "./components/ContactCard";
+import { teams } from "../../constants/contacts";
 
 export default function Contact() {
-    return (
-        <PageLayout title={"Contact"} imgUrl={"/ambassador.png"}>
+  return (
+    <PageLayout title={"Contact"} imgUrl={"/ambassador.png"}>
+      {/* Ambassador team  */}
+      <div>
+        <div className="text-5xl font-bold font-neotriad textShadow-md text-white my-8">
+          Ambassador Team
+        </div>
+        <div className="px-5 py-8 flex gap-8 flex-wrap items-center overflow-hidden justify-center">
+          {teams.ambassador.map((team, index) => (
+            <ContactCard key={index} {...team} />
+          ))}
+        </div>
+      </div>
 
-        </PageLayout>
-    )
+      {/* events team  */}
+      <div>
+        <div className="text-5xl font-bold font-neotriad textShadow-md text-white my-8">
+          Event Management Team
+        </div>
+        <div className="px-5 py-8 flex gap-8 flex-wrap items-center overflow-hidden justify-center">
+          {teams.eventManagement.map((team, index) => (
+            <ContactCard key={index} {...team} />
+          ))}
+        </div>
+      </div>
+
+      {/* web dev team  */}
+      <div>
+        <div className="text-5xl font-bold font-neotriad textShadow-md text-white my-8">
+          Web Dev Team
+        </div>
+        <div className="px-5 py-8 flex gap-8 flex-wrap items-center overflow-hidden justify-center">
+          {teams.webDev.map((team, index) => (
+            <ContactCard key={index} {...team} />
+          ))}
+        </div>
+      </div>
+    </PageLayout>
+  );
 }
