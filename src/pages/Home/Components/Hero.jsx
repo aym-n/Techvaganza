a364d3dd5ab7f8f22741f202960a44642653833c
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-09-20 21:01:59 +0530

    changes in hero

--- src/pages/Home/Components/Hero.jsx
+++ src/pages/Home/Components/Hero.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef,useCallback } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import Header from "../../../Components/Header";
 import { Link } from "react-router-dom";
 import Aos from "aos";
@@ -49,7 +49,7 @@ function Hero() {
     return () => clearTimeout(timer);
   });
 
-// scrambling text animation 
+  // scrambling text animation
   const [scrambledText, setScrambledText] = useState("Techvaganza");
   const textRef = useRef(null);
   const letters = "abcdefghijklmnopqrstuvwxyz";
@@ -58,13 +58,16 @@ function Hero() {
   const scrambleText = useCallback(() => {
     let iteration = 0;
     const interval = setInterval(() => {
-      setScrambledText(prev => 
-        prev.split("").map((letter, index) => {
-          if (index < iteration) {
-            return originalText[index];
-          }
-          return letters[Math.floor(Math.random() * 26)];
-        }).join("")
+      setScrambledText((prev) =>
+        prev
+          .split("")
+          .map((letter, index) => {
+            if (index < iteration) {
+              return originalText[index];
+            }
+            return letters[Math.floor(Math.random() * 26)];
+          })
+          .join("")
       );
 
       if (iteration >= originalText.length) {
@@ -94,91 +97,107 @@ function Hero() {
   }, []);
 
   return (
-    <div
-      className="bg-cover bg-center  min-h-screen"
-      style={{
-        backgroundImage:
-          "linear-gradient(to bottom, #000000 0%, #00000044 50%, #00000033 100%), url(/common/hero-cover.jpeg)",
-      }}
-    >
-      <div>
-        <Header />
-      </div>
-      <div className="pt-4 md:pt-32 p-7 md:p-32">
-        <div
-          data-aos="fade-down"
-          data-aos-duration="2000"
-          className="text-white text-xl ss:text-2xl md:text-4xl text-center overflow-visible xs:whitespace-nowrap"
-        >
-          10
-          <sup className="text-white text-xl md:text-2xl">th </sup>& 11
-          <sup className="text-white text-xl md:text-2xl">th </sup>
-          Oct 2024
+    <div className="relative min-h-screen overflow-hidden">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute z-0 w-full h-full object-cover"
+      >
+        <source src="/common/hero.mp4" type="video/mp4" />
+        {/* Fallback content for browsers that don't support video */}
+        <img
+          src="/common/hero-cover.jpeg"
+          alt="Background"
+          className="absolute z-0 w-full h-full object-cover"
+        />
+      </video>
+
+      {/* Overlay */}
+      {/* <div className="absolute z-10 inset-0 bg-gradient-to-b from-black via-black/70 to-black/50"></div> */}
+
+      <div className="relative z-20">
+        <div>
+          <Header />
         </div>
-        <p 
-          ref={textRef}
-          className="text-center text-5xl ss:text-7xl sm:text-8xl md:text-9xl mt-4 text-white font-neotriad font-extrabold overflow-visible textShadow"
-        >
-          {scrambledText}
-        </p>
+        <div className="pt-4 md:pt-32 p-7 md:p-32">
+          <div
+            data-aos="fade-down"
+            data-aos-duration="2000"
+            className="text-white text-xl ss:text-2xl md:text-4xl text-center overflow-visible xs:whitespace-nowrap"
+          >
+            10
+            <sup className="text-white text-xl md:text-2xl">th </sup>& 11
+            <sup className="text-white text-xl md:text-2xl">th </sup>
+            Oct 2024
+          </div>
+          <p
+            ref={textRef}
+            className="text-center text-5xl ss:text-7xl sm:text-8xl md:text-9xl mt-4 text-white font-neotriad font-extrabold overflow-visible textShadow"
+          >
+            {scrambledText}
+          </p>
 
-        <p 
-          className="text-center text-2xl ss:text-3xl  mt-4 text-white font-kodeMono font-bold overflow-visible textShadow-sm"
-        >
-          Greener Innovations: Engineering a Sustainable World
-        </p>
+          <p className="text-center text-2xl ss:text-3xl  mt-4 text-white font-kodeMono font-bold overflow-visible textShadow-sm">
+            Greener Innovations: Engineering a Sustainable World
+          </p>
 
-        <div className="font-kodeMono mt-6 md:mt-10 flex justify-center">
-          <div className="w-full min-w-150 mt-7 px-12 lg:px-40 flex flex-wrap gap-6 justify-between text-white font-bold overflow-hidden">
-            <div data-aos="fade-down" className="text-center overflow-visible">
-              <div className="block text-6xl md:text-9xl font-semibold overflow-visible">
-                {timeLeft.days}
+          <div className="font-kodeMono mt-6 md:mt-10 flex justify-center">
+            <div className="w-full min-w-150 mt-7 px-12 lg:px-40 flex flex-wrap gap-6 justify-between text-white font-bold overflow-hidden">
+              <div
+                data-aos="fade-down"
+                className="text-center overflow-visible"
+              >
+                <div className="block text-6xl md:text-9xl font-semibold overflow-visible">
+                  {timeLeft.days}
+                </div>
+                <div className=" block text-lg md:text-3xl overflow-visible textShadow-sm">
+                  DAYS
+                </div>
               </div>
-              <div className=" block text-lg md:text-3xl overflow-visible textShadow-sm">
-                DAYS
+              <div
+                data-aos="fade-down"
+                data-aos-delay="100"
+                className="text-center overflow-visible"
+              >
+                <div className="block text-6xl md:text-9xl font-semibold overflow-visible">
+                  {timeLeft.hours}
+                </div>
+                <div className="block text-lg md:text-3xl overflow-visible textShadow-sm">
+                  HOURS
+                </div>
               </div>
-            </div>
-            <div
-              data-aos="fade-down"
-              data-aos-delay="100"
-              className="text-center overflow-visible"
-            >
-              <div className="block text-6xl md:text-9xl font-semibold overflow-visible">
-                {timeLeft.hours}
+              <div
+                data-aos="fade-down"
+                data-aos-delay="200"
+                className="text-center overflow-visible"
+              >
+                <div className="block text-6xl md:text-9xl font-semibold overflow-visible">
+                  {timeLeft.minutes}
+                </div>
+                <div className=" block text-lg md:text-3xl overflow-visible textShadow-sm">
+                  MINUTES
+                </div>
               </div>
-              <div className="block text-lg md:text-3xl overflow-visible textShadow-sm">
-                HOURS
-              </div>
-            </div>
-            <div
-              data-aos="fade-down"
-              data-aos-delay="200"
-              className="text-center overflow-visible"
-            >
-              <div className="block text-6xl md:text-9xl font-semibold overflow-visible">
-                {timeLeft.minutes}
-              </div>
-              <div className=" block text-lg md:text-3xl overflow-visible textShadow-sm">
-                MINUTES
-              </div>
-            </div>
-            <div
-              data-aos="fade-down"
-              data-aos-delay="300"
-              className="text-center overflow-visible"
-            >
-              <div className="block text-6xl md:text-9xl font-semibold overflow-visible">
-                {timeLeft.seconds}
-              </div>
-              <div className=" block text-lg md:text-3xl overflow-visible textShadow-sm">
-                SECONDS
+              <div
+                data-aos="fade-down"
+                data-aos-delay="300"
+                className="text-center overflow-visible"
+              >
+                <div className="block text-6xl md:text-9xl font-semibold overflow-visible">
+                  {timeLeft.seconds}
+                </div>
+                <div className=" block text-lg md:text-3xl overflow-visible textShadow-sm">
+                  SECONDS
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="flex flex-col items-center justify-center md:flex-row gap-8 md:gap-7 my-10 md:mt-8">
-          <p className="flex gap-4 items-center text-white textShadow-md md:textShadow font-figtree text-md md:text-3xl mt-5 ">[ Event Registrations Opening Soon ]</p>
-          {/* <div className="flex gap-4 items-center text-secondary">
+          <div className="flex flex-col items-center justify-center md:flex-row gap-8 md:gap-7 my-10 md:mt-8">
+            <p className="flex gap-4 items-center text-white textShadow-md md:textShadow font-figtree text-md md:text-3xl mt-5 ">
+              [ Event Registrations Opening Soon ]
+            </p>
+            {/* <div className="flex gap-4 items-center text-secondary">
             <FaAddressCard size={24} />
             <Link
               data-aos="fade-in"
@@ -189,13 +208,14 @@ function Hero() {
               [ Register Yourself ]
             </Link>
           </div> */}
-          {/* <Link
+            {/* <Link
             data-aos="fade-in"
             data-aos-delay="500"
             className="rounded-full bg-primary text-secondary font-semibold py-4 px-10 font-kodeMono custom-glow"
           >
             Attendee Registration
           </Link> */}
+          </div>
         </div>
       </div>
     </div>
